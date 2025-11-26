117ba69fefc063498d66251fe79c403bf2938fef
Progyan Sen <progyansen@gmail.com>
2025-11-26 11:38:25 +0530

    add genres routes

--- src/index.js
+++ src/index.js
@@ -13,11 +13,49 @@ app.get('/isRunning',(req,res) => {
 
 app.post("/api/v1/genres", async (req,res) => {
     const { name } = req.body;
+    const existingName = await prisma.genre.findFirst({
+        where : {name : name}
+    })
+    if (existingName) {
+        res.status(400).json({error : "this genre is exist"});
+    }
     const newGenre = await prisma.genre.create({
         data : {name}
     })
     res.status(201).json(newGenre)
 });
+
+app.get("/api/v1/genres", async(req,res) => {
+    const genres = await prisma.genre.findMany()
+    res.status(200).json(genres)
+})
+
+app.get("/api/v1/genres/:id",async (req,res) => {
+    const {id} = req.params
+    const genreById = await prisma.genre.findUnique({
+        where : {id : Number(id)}
+    })
+    res.status(200).json(genreById)
+})
+
+app.patch("/api/v1/genres/:id", async(req,res) => {
+    const {id} = req.params
+    const {name} = req.body
+    const updateGenre = await prisma.genre.update({
+        where : {id: Number(id)},
+        data : {name}
+    })
+    res.status(200).json(updateGenre)
+})
+
+app.delete("/api/v1/genres/:id", async(req,res) => {
+    const {id} = req.params
+    const deleteGenre = await prisma.genre.delete({
+        where : {id : Number(id)}
+    })
+    res.status(204).json("deleted geners sucessfully")
+});
+
 
 app.post("/api/v1/authors", async (req,res) => {
     const {name, dob} = req.body;
