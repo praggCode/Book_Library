69c09ae3fa9b9882e86a0816abb408dc04585e8e
Progyan Sen <progyansen@gmail.com>
2025-11-27 11:47:07 +0530

    feat: author route created

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ app.get('/isRunning',(req,res) => {
     res.send("server is running")
 })
 
+// Genre Routes
+
 app.post("/api/v1/genres", async (req,res) => {
     const { name } = req.body;
     const existingName = await prisma.genre.findFirst({
@@ -56,16 +58,53 @@ app.delete("/api/v1/genres/:id", async(req,res) => {
     res.status(204).json("deleted geners sucessfully")
 });
 
+// Author Routes
+
 
 app.post("/api/v1/authors", async (req,res) => {
-    const {name, dob} = req.body;
+    const {name, DOB} = req.body;
+    const existingName = await prisma.author.findFirst({
+        where : {name : name}
+    })
+    if (existingName) {
+        res.status(400).json({error : "this author is exist"});
+    }
     const newAuthor = await prisma.author.create({
-      data: { name: name, DOB: new Date(dob) },
+      data: { name: name, DOB: new Date(DOB) },
     });
     res.status(201).json(newAuthor)
 })
 
+app.get("/api/v1/authors", async(req,res) => {
+    const authors = await prisma.author.findMany()
+    res.status(200).json(authors)
+})
 
+app.get("/api/v1/authors/:id",async (req,res) => {
+    const {id} = req.params
+    const authorById = await prisma.author.findUnique({
+        where : {id : Number(id)}
+    })
+    res.status(200).json(authorById)
+})
+
+app.patch("/api/v1/authors/:id", async(req,res) => {
+    const {id} = req.params
+    const {name, DOB} = req.body
+    const updateAuthor = await prisma.author.update({
+        where : {id: Number(id)},
+        data : {name, DOB: new Date(DOB)}
+    })
+    res.status(200).json(updateAuthor)
+})
+
+app.delete("/api/v1/authors/:id", async(req,res) => {
+    const {id} = req.params
+    const deleteAuthor = await prisma.author.delete({
+        where : {id : Number(id)}
+    })
+    res.status(204).json(deleteAuthor)
+});
 
 app.listen(3005,() => {
     console.log("Server is running on port 3005")
